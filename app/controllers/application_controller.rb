e198672c3fdaeba64cf5d8a9cd82223bccd1b26d
sushma <sushmahpandey@gmail.com>
2011-01-13 21:58:50 +0530

    partial fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,44 @@ require 'httparty'
 
 class ApplicationController < ActionController::Base
   #protect_from_forgery
-  layout false
+  respond_to :xml, :json
+
+  def rescue_action(e)
+    puts "--------------------------------------In here"
+  end
+
+  def catch_all
+    #get_url(params[:url])
+    @response = HTTParty.get(session[:domain] + "/" + params[:path])
+    @response= @response.html_safe
+    render :template => "application/proxify"
+  end
 
   def proxify
-    requested_url = params[:url]
-    if request.post?
-      response = HTTParty.post(requested_url, :query => params)
-    else
-      response = HTTParty.get(requested_url)
-    end
+    get_url(params[:url], params)
+    urls = params[:url].split("/")
+    session[:domain] =  urls[0..2].join('/')
+    puts "--------#{urls[0..2].join('/')}"
+     #@response = @response.html_safe unless @response.headers.has_value?("text/javascript")
+#    puts "***********#{@response.headers["content-type"].split('/').last.split(';').first}"
+     
+    #respond_with(@response)
     respond_to do |format|
-      format.xml  { render :xml => response}
-      format.json { render :json => response}
+      format.html{}
+      format.json{render :json=> @response}
+      format.xml{render :xml=> @response}
     end
+  end
+
+  def get_url(url, post_params = nil)
+    if request.post?
+      @response = HTTParty.post(url, :query => post_params)
+    else
+      @response = HTTParty.get(url)
+    end
+    @resp_format = @response.headers["content-type"].split('/').last.split(';').first
+    re=  @resp_format == 'html' ? @response.html_safe : (@resp_format == 'javascript' ? @response.to_json.html_safe : @response.to_xml.html_safe)
+    @response = re
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,8 @@ Proxify::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => "application#proxify"
-
+   get '*path' => "application#catch_all"
+   #match '*path' => "application#proxify"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
