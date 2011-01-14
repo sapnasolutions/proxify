20cb18380f2c8b2510dca3a11ee85d2ae02c778c
sushma <sushmahpandey@gmail.com>
2011-01-14 17:26:57 +0530

    code refactored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,31 +4,22 @@ class ApplicationController < ActionController::Base
 
   def catch_routes
     response = HTTParty.get(session[:domain] + "/" + params[:path])
-    send_data(response, :type =>  response.headers["content-type"].to_s, :disposition  =>  'inline')
+    send_data(response, :type =>  response.headers["content-type"], :disposition  =>  'inline')
   end
 
   def proxify
-    unless params[:url]
-      @response = "Welcome to proxify"
-      render :template => "application/index"
-      return
-    end
-
+    (render :template => "application/index"; return) unless params[:url]
+    
     url = params[:url]
     url = "http://" + url if(url.index(/\b(?:https?:\/\/)\S+\b/) == nil)
     get_url(url, params)
     session[:domain] =  url.match(/\b(?:https?:\/\/)\S+\b/).to_s
     
     send_data(@response, :type =>  "xml", :disposition  =>  'inline') if @response_format == "xml"
-    
   end
 
   def get_url(url, post_params = nil)
-    if request.post?
-      @response = HTTParty.post(url, :query => post_params)
-    else
-      @response = HTTParty.get(url)
-    end
+    @response = request.post? ? HTTParty.post(url, :query => post_params) : HTTParty.get(url)
     @response_format = @response.headers["content-type"].split('/').last.split(';').first
     @response =  @response_format == 'html' ? @response.html_safe : (@response_format == 'javascript' ? @response.to_json.html_safe : @response.to_xml)
   end
